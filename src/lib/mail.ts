a64b1143ec99a0ac431d714cec78ac145de40f62
Amirhossein-Ra <000amir111rahzany@gmail.com>
2025-01-24 22:38:02 +0330

    debugging

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_123");
 
 export async function sendVerificationEmail(email: string, token: string) {
   const link = `http://localhost:3000/verify-email?token=${token}`;
